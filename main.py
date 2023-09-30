c2b191f5fe6c12bff787c539c3179004eecb566b
Alanis <146586232+Alanis24Marjorie@users.noreply.github.com>
2023-09-30 17:52:30 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+# horas=input('que horas são')
+# minutos=input('que minutos são')
+# segundos=input('que segundos são')
+# print(f'são-{horas} horas,é-{minutos} minutos,é-{segundos} segundos')
+
+# ingrediente1='farinha'
+# ingrediente2='ovo'
+# ingrediente3='água'
+# print(f'{ingrediente1},{ingrediente2},{ingrediente3}')
+
+
+# senha_digitada = input('Digite uma senha')
+# nome = input('Digite seu nome')
+# senha = '1234'
+
+# if senha == senha_digitada and nome== 'Alanis':
+#    print('Acesse, voce digitou corretamente')
+# else:
+#    print(f'Não pode acessar, voce digitou {senha_digitada} ou {nome} incorretamete')
+
+# class Operacao:
+#     def condicao():
+#       if 2 == 2:
+#         print("É igual")
+#       else:
+#         print('É diferente')
+
+
+# age=int(input('qual dua idade'))
+# if age>=16 and age<=65:
+#    print('voce já esta na idade de votar')
+# else:
+#    print('voce não pode votar')
+
+# if nota == 10:
+#    print('voce passou de ano')
+# elif {nota} >=7 and nota <=9:
+#    print('voce se saiu bem')
+# else:
+#    print("deixa para a próxima")
+  
